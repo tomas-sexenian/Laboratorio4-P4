ffce30e69dcc927dd9169b3f3001990849067c6d
PaulaC04 <100801800+PaulaC04@users.noreply.github.com>
2022-06-13 00:11:22 -0300

    Update HabitacionController.cpp

--- src/HabitacionController.cpp
+++ src/HabitacionController.cpp
@@ -29,9 +29,34 @@ void HabitacionController::setHabitacion() {
 
 // DE ACA HACIA ABAJO IMPLEMENTAN LAS OPERACIONES
 
+void HabitacionController :: ingresarDatosHabitacion(Habitacion* datosHabitacion) {
+    this->habitacionIniciar = datosHabitacion;
+}
+
+void HabitacionController :: confirmarAltaHabitacion() {
+    this->Habitaciones.insert(pair<int,Habitacion*>(habitacionIniciar->getNumero(),new Habitacion(
+        habitacionIniciar->getNumero(), habitacionIniciar->getPrecio(), habitacionIniciar->getCapacidad(), 
+        habitacionIniciar->getHostal(), habitacionIniciar->getReservas())));
+    this->habitacionIniciar = NULL;
+}
+
+void HabitacionController :: cancelarAltaHabitacion() {
+     this->habitacionIniciar = NULL;
+};
+
+map<int,Habitacion*> HabitacionController :: getHabitaciones() {
+    return this->Habitaciones;
+};
+
+
+Habitacion * HabitacionController :: getHabitacionIniciar() {
+    return this->habitacionIniciar; 
+};
+
+
+
 list<DTHabitacion> HabitacionController::obtenerHabitacionesDisponiblesHostal(string NombreHostal, DTFecha checkInt, DTFecha checkOut) {
 }
 
 void HabitacionController::seleccionarHabitacion(int NumeroHabitacion) {
 }
-
